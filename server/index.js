d0dff3cbeae3d3dcc2e5ce407945831a8f917054
fq-admin <admin@finquotient.com>
2024-05-30 00:46:31 +0530

    dist add

--- server/index.js
+++ server/index.js
@@ -5,9 +5,13 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { UserRouter } from './routes/user.js'
+import path from 'path'
 
 const app = express()
 app.use(express.json())
+const _dirname=path.dirname("")
+const buildpath=path.join(_dirname,"../client/dist")
+app.use(express.static(buildpath))
 app.use(cors(
     {
         origin: ["http://localhost:5173"],
